9ea76a95060cc7599cc881d36a15f3a786224de8
Shrey_sadhukhan <shrey.sadhukhan21@gmail.com>
2024-05-19 14:53:18 +0530

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/logo.svg" type="image/x-icon" />
+      </head>
       <body className={inter.className}>
         <ConvexClientProvider>
           {children}
